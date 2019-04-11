cd62cceaaedee4619e6d7cc9738a85aef1ae24f9
Mike <michael.anthony.stack@gmail.com>
2019-04-11 00:31:00 -0400

    RESET DEV

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
+import logo from '@assets/logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(): JSX.Element {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
