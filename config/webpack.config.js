4ed4b460f25722650e829c2f5ccb799360dcdb13
Michael Stack <Michael.Anthony.Stack@gmail.com>
2020-01-02 10:49:18 -0500

    stash changes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -348,7 +348,7 @@ module.exports = function(webpackEnv) {
               test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
               loader: require.resolve("url-loader"),
               options: {
-                limit: 10000,
+                limit: imageInlineSizeLimit,
                 name: "static/media/[name].[hash:8].[ext]",
               },
             },
